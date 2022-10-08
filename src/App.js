a5d2d2d69bac42ca9bf87963b2448db721f9910d
Its-JoeTheKing <errfig22@gmail.com>
2022-10-08 22:39:10 +0100

    yup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState,useEffect} from 'react';
-
+import {quotes} from './quotes'
 function App() {
   return (
     <div className="App">
@@ -8,7 +8,7 @@ function App() {
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true} />
       <link href="https://fonts.googleapis.com/css2?family=Acme&family=Bangers&family=Bree+Serif&family=Josefin+Sans&family=Montserrat:wght@500&family=Poppins:wght@500&family=Roboto:wght@500&family=Secular+One&family=Signika+Negative&family=Staatliches&family=Teko&display=swap" rel="stylesheet"/> 
       <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossOrigin="anonymous"/>
-      
+
     </div>
   );
 }
